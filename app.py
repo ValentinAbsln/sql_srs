ab9ce330a1eba93110682b80db77c957fc32fb20
Valentin <val_440@outlook.fr>
2024-01-19 22:47:48 +0100

    premier commit test

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+from pandas import DataFrame
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+with tab1:
+    input_text = st.text_area(label="Entrez votre texte")
+    tmp = duckdb.query(input_text).df()
+    st.write(f"Votre requête est la suivante :  {input_text}")
+    st.dataframe(tmp)
+with tab2:
+    st.header("A dog")
+
+with tab3:
+    st.header("A Owl")
