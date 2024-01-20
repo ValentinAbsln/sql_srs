1d47ccefe7e9276ece9d772b5416e856506c7d15
Valentin <val_440@outlook.fr>
2024-01-20 12:17:43 +0100

    add features selectbox

--- app.py
+++ app.py
@@ -6,9 +6,20 @@ from pandas import DataFrame
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
+
+st.write("App SQL practice")
+
+option = st.selectbox("What would u like to review ?",
+                      ["Joins", "Group by", "Windows Functions"],
+                      index=None,
+                      placeholder="Select a theme "
+                      )
+
+st.write('You selected : ', option)
+
 with tab1:
     input_text = st.text_area(label="Entrez votre texte")
-    tmp = duckdb.query(input_text).df()
+    tmp = duckdb.query(input_text)
     st.write(f"Votre requête est la suivante :  {input_text}")
     st.dataframe(tmp)
 with tab2:
