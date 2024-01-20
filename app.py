e780933213e9314414e2bdcd84c97d1695579705
ValentinAbsln <155489560+ValentinAbsln@users.noreply.github.com>
2024-01-20 18:58:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,32 @@
-import io
-
 import streamlit as st
 import pandas as pd
 import duckdb
-import io
 from pandas import DataFrame
 
+st.write("App SQL practice")
+with st.sidebar:
+    option = st.selectbox("What would u like to review ?",
+                            ["Joins", "Group by", "Windows Functions"],
+                          index=None,
+                          placeholder="Select a theme "
+                          )
+    st.write('You selected : ', option)
 
-csv='''
-beverage,price
-orange juice, 2.5
-Expression, 2
-Tea, 3
-'''
 
-beverages = pd.read_csv(io.StringIO(csv))
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+data = {"a": [1, 2, 3], "b": [4, 5, 6] }
+df = pd.DataFrame(data)
 
-csv2 ='''
-food_item,food_price
-cookie juice, 2.5
-chocolatine, 2
-muffin, 3'''
 
-food_items = pd.read_csv(io.StringIO(csv2))
 
-answer = """
-SELECT * FROM beverages 
-CROSS JOIN food_items"""
 
-solutions = duckdb.sql(answer).df()
-
-st.header("enter your code:")
-
-query = st.text_area(label="votre code SQL ici,", key="user_input")
-
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-tab2, tab3 = st.tabs(["Tables","Solutions"])
-
+with tab1:
+    input_text = st.text_area(label="Entrez votre texte")
+    tmp = duckdb.query(input_text)
+    st.write(f"Votre requête est la suivante :  {input_text}")
+    st.dataframe(tmp)
 with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected")
-    st.dataframe(solutions)
+    st.header("A dog")
 
 with tab3:
-    st.write(answer)
+    st.header("A Owl")
