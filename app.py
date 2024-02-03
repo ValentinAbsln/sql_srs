62d10056529678553296b786a239502071e91e0e
Valentin <val_440@outlook.fr>
2024-02-03 11:53:59 +0100

    add result difference

--- app.py
+++ app.py
@@ -24,30 +24,38 @@ with st.sidebar:
     exercice = con.execute(f"SELECT * FROM memory_state where theme = '{theme}'").df()
 
     st.write(exercice)
+    try:
+        exercice_name = exercice.loc[0, "exercice_name"]
+        with open(f"answer/{exercice_name}.sql", "r") as f:
+            answer = f.read()
+            solutions_df = con.execute(answer).df()
+    except KeyError as e:
+        st.write("Select exercice")
+
 
 st.header("enter your code:")
 
 query = st.text_area(label="votre code SQL ici,", key="user_input")
 #
 if query:
-     result = con.execute(query).df()
-     st.dataframe(result)
-#
-#     if len(result.columns) != len(solutions_df.columns):
-#         st.write("Some columns are missing")
-#     try:
-#         result = result[solutions_df.columns]
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#     n_lines_difference = result.shape[0] - solutions_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f" result has {n_lines_difference} lines difference with the solution)"
-#         )
-#
-#     st.dataframe(result.compare(solutions_df))
-#
-#
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+    if len(result.columns) != len(solutions_df.columns):
+        st.write("Some columns are missing")
+    try:
+        result = result[solutions_df.columns]
+    except KeyError as e:
+        st.write("Some columns are missing")
+        n_lines_difference = result.shape[0] - solutions_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f" result has {n_lines_difference} lines difference with the solution"
+        )
+
+    # st.dataframe(result.compare(solutions_df))
+
+
 # st.write("App SQL practice")
 #
 #
@@ -55,22 +63,23 @@ tab2, tab3 = st.tabs(["Tables", "Solutions"])
 #
 #
 with tab2:
-      exercice_tables = ast.literal_eval(exercice.loc[0,"tables"])
+    try:
+        exercice_tables = ast.literal_eval(exercice.loc[0, "tables"])
 
-      for table in exercice_tables:
-          st.write(f"table: {table}")
-          print(f"{table}")
-          df_table = con.execute(f"SELECT * FROM {table}").df()
-          st.dataframe(df_table)
-
+        for table in exercice_tables:
+            st.write(f"table: {table}")
+            print(f"{table}")
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
+    except KeyError as k:
+        st.write("Select exercice")
 #     st.dataframe(food_items)
 #     st.write("expected")
 #     st.dataframe(solutions_df)
 #
 with tab3:
-    exercice_name = exercice.loc[0, "exercice_name"]
-    with open(f"answer/{exercice_name}.sql" ,"r") as f:
-        answer= f.read()
-    st.write(answer)
 
-
+    try:
+        st.write(answer)
+    except NameError as n:
+        st.write("Select exercice")
